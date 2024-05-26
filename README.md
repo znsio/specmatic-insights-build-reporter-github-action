492b285eb81a6cbf33ae34b454b6ec2d52d19a98
Naresh Jain <nashjain@gmail.com>
2024-05-26 16:03:52 +0530

    Implemented the github action

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# specmatic-insights-build-reporter-github-action
-Github Action for Specmatic Insights
+# Specmatic Insights Github Action
+This Github Action is used to publish the build stats to the Specmatic Insights server.
+
+While invoking this Github Action, the following arguments must be passes:
+
+- `specmatic-insights-host`(Required) URL of the Specmatic Insights server. Ex: https://insights.specmatic.in
+- `specmatic-central-repo-report`(Optional) Path to the central contract repo report. Ex: ./build/reports/specmatic/central_contract_repo_report.json
+- `specmatic-coverage`(Optional) Path to the coverage report. Ex: ./build/reports/specmatic/coverage_report.json
+- `specmatic-stub-usage`(Optional) Path to the stub usage report. Ex: ./build/reports/specmatic/stub_usage_report.json
---
+++ index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const { exec } = require('child_process');
+
+async function run() {
+    console.log('Running Specmatic Insights Build Reporter for Github Action');
+    try {
+        const argsString = Object.keys(process.env)
+            .filter(key => key.startsWith('INPUT_'))
+            .reduce((acc, key) => {
+                const inputName = key.slice('INPUT_'.length).toLowerCase();
+                return `${acc} --${inputName}=${process.env[key]}`;
+            }, '');
+
+        const command = `npx specmatic-insights-github-build-reporter ${argsString}`;
+
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                core.setFailed(`Action failed with error: ${error.message}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+        });
+    } catch (error) {
+        core.setFailed(`Action failed with error: ${error.message}`);
+    }
+}
+
+run();
